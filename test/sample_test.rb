d7661a4f9710653c966493e625f8f9ffbc3f5820
Reltre <tglippert@gmail.com>
2018-06-07 15:29:57 -0700

    Add libraries and boilerplate test file.

---
+++ test/sample_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+
+class TestSample < Minitest::Test
+  def setup
+    # setup
+  end
+
+  def test_certain_behavior
+    # exercise
+    # assert
+  end
+
+  def teardown
+    # teardown
+  end
+end
